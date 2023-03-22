ec80e98099942023d992f5efb9232a89d61cdf69
Francesco Amarena <ungisk92@gmail.com>
2023-03-22 15:35:13 +0100

    Initial commit

---
+++ spring-repositories-1/src/main/java/co/develhope/springrepositories1/entities/Car.java
@@ -0,0 +1,27 @@
+package co.develhope.springrepositories1.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "model_name", nullable = false)
+    private String modelName;
+
+    @Column(name = "serial_number", nullable = false)
+    private String serialNumber;
+
+    @Column(name = "current_price")
+    private Double currentPrice;
+}
